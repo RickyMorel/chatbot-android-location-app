bbf17469c0b82f4f2b341c5dcd369dc3e17f952c
Ricky_Mexx <ricardomorel0903@gmail.com>
2024-08-22 12:44:50 -0400

    commit

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Bookmark extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Bookmark</Text>
+      </View>
+    )
+  }
+}
+
+export default Bookmark
---
+++ components/CustomButton.jsx
@@ -0,0 +1,21 @@
+import { TouchableOpacity, Text } from 'react-native'
+import React, { Component } from 'react'
+
+export class CustomButton extends Component {
+  render() {
+    const {title, handlePress, containerStyles, textStyles, isLoading} = this.props
+
+    return (
+      <TouchableOpacity 
+        className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`} 
+        onPress={handlePress}
+        activeOpacity={0.7}
+        disabled={isLoading}
+    >
+        <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>{title}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+export default CustomButton

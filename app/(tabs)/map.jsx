ea934ec742872a730b795d76b29512d6978837c3
Ricky_Mexx <ricardomorel0903@gmail.com>
2024-08-24 11:31:21 -0400

    commit

--- app/(tabs)/map.jsx
+++ app/(tabs)/map.jsx
@@ -1,12 +1,56 @@
-import { Text, View } from 'react-native'
+import { Text, View, FlatList, RefreshControl, Image } from 'react-native'
 import React, { Component } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import MapComponent from '../../components/MapComponent'
+import { images } from '../../constants'
+import CardsView from '../../components/CardsView'
 
 export class Map extends Component {
+  constructor() {
+    super()
+    this.state = {
+      refreshing: false,
+      isLoading: false,
+      data: [],
+    }
+  }
+
   render() {
     return (
       <View>
         <MapComponent/>
+        <SafeAreaView className="bg-primary h-full">
+          <FlatList 
+            data={this.state?.data} 
+            keyExtractor={(item) => item.id} 
+            renderItem={({item}) => (
+              <VideoCard video={item}/>
+            )}
+            ListHeaderComponent={() => (
+              <View className="my-6 px-4 space-y-6">
+                <View className="justify-between items-start flex-row mb-6">
+                  <View>
+                    <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
+                    <Text className="text-2xl font-psemibold text-white">JSMastery</Text>
+                  </View>
+
+                  <View className="mt-1.5">
+                    <Image source={images.logoSmall} className="w-9 h-10" resizeMode='contain'/>
+                  </View>
+                </View>
+
+                <View className="w-full flex-1 pt-5 pb-8">
+                  <Text className="text-gray-100 text-lg font-pregular mb-3">Latest Videos</Text>
+                  <CardsView posts={this.state?.data ?? []}/>
+                </View>
+              </View>
+            )}
+            ListEmptyComponent={() => (
+              <EmptyState title="No videos found" subtitle="Be the first one to upload a video!"/>
+            )}
+            refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh}/>}
+          />
+        </SafeAreaView>
       </View>
     )
   }
---
+++ components/CardsView.jsx
@@ -0,0 +1,81 @@
+import { FlatList, ImageBackground, Text, View, TouchableOpacity, Image } from 'react-native'
+import React, { Component } from 'react'
+import * as Animatable from 'react-native-animatable'
+import { icons } from '../constants'
+
+const zoomIn = {
+  0: {
+    scale: 0.9
+  },
+  1: {
+    scale: 1.1
+  }
+}
+
+const zoomOut = {
+  0: {
+    scale: 1.1
+  },
+  1: {
+    scale: 0.9
+  }
+}
+
+const TrendingItem = ({activeItem, item}) => {
+  console.log("activeItem", activeItem)
+  console.log("item", item)
+  return (
+    <Animatable.View className="mr-5" animation={activeItem == item.id ? zoomIn : zoomOut} duration={500}>
+      <TouchableOpacity activeOpacity={0.7} className="relative justify-center items-center">
+        <ImageBackground source={{uri: item.thumbnail}} className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black-40" resizeMode='cover'/>
+        <Image source={icons.play} className="w-12 h-12 absolute" resizeMode='contain'/>
+      </TouchableOpacity>
+    </Animatable.View>
+  )
+}
+
+export class CardsView extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      activeItem: undefined
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      activeItem: this.props.posts[0]
+    })
+  }
+
+  onViewableItemsChanged = ({viewableItems}) => {
+    if(viewableItems.length > 0) {
+      this.setState({
+        activeItem: viewableItems[0].key
+      })
+    }
+  }
+
+  render() {
+    const {posts} = this.props
+
+    return (
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => (
+            <TrendingItem activeItem={this.state.activeItem} item={item}/>
+        )}
+        horizontal
+        onViewableItemsChanged={this.onViewableItemsChanged}
+        viewabilityConfig={{
+          itemVisiblePercentThreshold: 70
+        }}
+        contentOffset={{x: 170}}
+      />
+    )
+  }
+}
+
+export default CardsView

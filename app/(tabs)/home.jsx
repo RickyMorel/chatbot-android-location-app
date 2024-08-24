c3d291dc20b3ce3aeebeb2022337646e419e82f8
Ricky_Mexx <ricardomorel0903@gmail.com>
2024-08-24 11:21:04 -0400

    commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,49 +1,87 @@
-import { Text, View, FlatList, Image, RefreshControl, Alert } from 'react-native'
 import React, { Component } from 'react'
+import { FlatList, Image, RefreshControl, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../../constants'
+import EmptyState from '../../components/EmptyState'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
-import EmptyState from '../../components/EmptyState'
+import { images } from '../../constants'
+import makeRequest from '../../lib/useAppwrite'
 import { isLoading } from 'expo-font'
-import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
 
 export class Home extends Component {
   constructor() {
     super()
     this.state = {
       refreshing: false,
+      isLoading: false,
+      data: [],
     }
   }
 
   componentDidMount() {
-
+    this.fetchData()
   }
+
+  fetchData = async () => {
+    const response = [{
+      title: "How AI Shpaes Coding Future",
+      thumbnail: "https://miro.medium.com/v2/resize:fit:366/0*hQqXIZ3yjxTPjbQI.jpg",
+      creator: "Ricardo",
+      id: 1
+    },
+    {
+      title: "How Pikachu is the nest",
+      thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWPRSk2MjJEzjeeSnl_AhVQ2YfMty0kQRlpw&s",
+      creator: "Alvaro",
+      id: 2
+    },
+    {
+        title: "Greedfall best tips",
+      thumbnail: "https://i.redd.it/xqr4pa679mm31.jpg",
+      creator: "Juan",
+      id: 3
+    },
+    {
+      title: "Crazy space facts",
+      thumbnail: "https://wallpapers.com/images/hd/illustration-in-space-4k-phone-xuuhy1m9n43v2zi5.jpg",
+      creator: "Ivo",
+      id: 4
+    }]
+    
+    try {
+        //const response = await fn();
+        const data = response;  // Mocked data processing
+        this.setState({
+          data: data
+        })
+    } catch (err) {
+        Alert.alert("Error", err.message);
+    } finally {
+        this.setState({
+          isLoading: false
+        })
+    }
+};
 
   onRefresh = async () => {
     this.setState({
       refreshing: true
     })
+    this.fetchData()
     this.setState({
       refreshing: false
     })
   }
 
   render() {
-    const {data} = useAppwrite(() => ({
-      title: "How AI Shpaes Coding Future",
-      thumnail: "https://mfiles.alphacoders.com/100/thumb-1008659.png",
-      creator: "Ricardo"
-  }))
-    // console.log("DATA IN HOMEEEEEE", data)
-
     return (
       <SafeAreaView className="bg-primary h-full">
         <FlatList 
-          data={[]} 
+          data={this.state?.data} 
           keyExtractor={(item) => item.id} 
           renderItem={({item}) => (
-            <Text className="text-3xl text-white">{item.id}</Text>
+            <VideoCard video={item}/>
           )}
           ListHeaderComponent={() => (
             <View className="my-6 px-4 space-y-6">
@@ -62,7 +100,7 @@ export class Home extends Component {
 
               <View className="w-full flex-1 pt-5 pb-8">
                 <Text className="text-gray-100 text-lg font-pregular mb-3">Latest Videos</Text>
-                <Trending posts={[{id: 1}, {id: 2}, {id: 3}] ?? []}/>
+                <Trending posts={this.state?.data ?? []}/>
               </View>
             </View>
           )}

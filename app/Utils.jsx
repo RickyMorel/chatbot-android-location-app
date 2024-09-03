123ed36ba66a9fccb9fb976a20b98eef58545355
Ricky_Mexx <ricardomorel0903@gmail.com>
2024-09-02 20:41:06 -0400

    better orderCard

--- app/Utils.jsx
+++ app/Utils.jsx
@@ -2,6 +2,16 @@ class Utils {
     static formatCurrency(price) {
         return `${Intl.NumberFormat('de-DE').format(price)} gs`
     }
+
+    static getCutName(name, maxCharacterLength = 18) {
+        let itemName = name
+    
+        if(itemName?.length > maxCharacterLength) {
+            itemName = itemName.slice(0, 18) + "..."
+        }
+
+        return itemName
+    }
 }
 
 export default Utils
--- components/OrderCardView.jsx
+++ components/OrderCardView.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Modal, Text, Pressable, View, StyleSheet } from 'react-native';
+import { Modal, Text, Pressable, View, StyleSheet , FlatList} from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
 import CustomButton from './CustomButton';
 import Utils from '../app/Utils';
 import { useRouter } from 'expo-router';
+import CreateSaleItem from './CreateSaleItem';
 
 const OrderCardView = ({ currentOrder, isOpen, closeCallback }) => {
   const router = useRouter();
@@ -19,37 +20,66 @@ const OrderCardView = ({ currentOrder, isOpen, closeCallback }) => {
 
   const serializedData = encodeURIComponent(JSON.stringify(currentOrder));
 
+  const OrderCardViewItem = (newItem) => {
+    const item = newItem.item
+    const itemName = Utils.getCutName(item.name, 12)
+
+    console.log("itemmmm", item)
+
+    return (
+      <View className="flex-row bg-white rounded-lg shadow-lg p-2 border border-gray-200 items-center justify-between">
+        {/* <Image
+            source={{ uri: allItems?.find(x => x.code == item.code)?.imageLink }}
+            className="h-[50px] w-[50px] rounded-lg" // No margin needed for equal spacing
+        /> */}
+        <Text className="text-left text-black font-semibold truncate">{itemName}</Text>
+        <Text className="text-gray-600 text-center">{item.amount}</Text>
+        <Text className="text-gray-600 font-semibold text-center">{Utils.formatCurrency(item.price)}</Text>
+      </View>
+    )
+  }
+
   return (
-    <Modal
-      animationType="fade"
-      transparent={true}
-      visible={isOpen}
-      onRequestClose={closeCallback}
-    >
-      <View className="flex-1 justify-center items-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.25)' }}>
-        <View className="bg-white rounded-lg p-6">
-          <View className="flex-row items-center">
-            <Text className="flex-1 text-lg">{currentOrder?.name}</Text>
-            <Text className="text-right">Total: {Utils.formatCurrency(currentOrder?.totalSold)}</Text>
-          </View>
-          <Table className="w-[300px]">
-            <Row data={tableData.tableHead} style={styles.head} textStyle={styles.headText} />
-            <Rows data={tableData.tableData} textStyle={styles.text} />
-          </Table>
-          <View className="flex-row items-center justify-center mt-2">
-            <View className='mr-4 h-[40px] w-[130px]'>
-              <CustomButton 
-                title="Hacer Venta" 
-                handlePress={() => { router.push(`/create-sale?data=${serializedData}`); closeCallback(); }} 
-              />
-            </View>
-            <View className='ml-4 h-[40px] w-[90px]'>
-              <CustomButton title="Cerrar" handlePress={closeCallback} />
-            </View>
-          </View>
+<Modal
+  animationType="fade"
+  transparent={true}
+  visible={isOpen}
+  onRequestClose={closeCallback}
+>
+  <View className="flex-1 justify-center items-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.25)' }}>
+    <View className="bg-white rounded-lg p-3" style={{ maxHeight: '50%' }}>
+      <View className="flex-row items-center">
+        <Text className="flex-1 text-lg">{currentOrder?.name}</Text>
+        <Text className="text-right">Total: {Utils.formatCurrency(currentOrder?.totalSold)}</Text>
+      </View>
+      <View className='pt-3 pb-3'>
+        <FlatList
+          style={{flexGrow: 0}}
+          data={currentOrder?.order}
+          keyExtractor={(item) => item.code}
+          renderItem={({ item }) => (
+            <OrderCardViewItem item={item} />
+          )}
+          viewabilityConfig={{
+            itemVisiblePercentThreshold: 70
+          }}
+        />
+      </View>
+      <View className="flex-row items-center justify-center mt-2">
+        <View className='mr-4 h-[40px] w-[130px]'>
+          <CustomButton 
+            title="Hacer Venta" 
+            handlePress={() => { router.push(`/create-sale?data=${serializedData}`); closeCallback(); }} 
+          />
+        </View>
+        <View className='ml-4 h-[40px] w-[90px]'>
+          <CustomButton title="Cerrar" handlePress={closeCallback} />
         </View>
       </View>
-    </Modal>
+    </View>
+  </View>
+</Modal>
+
   );
 };
 
@@ -59,5 +89,4 @@ const styles = StyleSheet.create({
   headText: { fontSize: 15, fontWeight: 'bold', textAlign: 'center', verticalAlign: 'top' },
   text: { margin: 6, fontSize: 13, textAlign: 'center', verticalAlign: 'top' },
 });
-
 export default OrderCardView;
